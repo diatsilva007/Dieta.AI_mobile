c619b106a6118bc76894748a851c7b122a85c685
diatsilva007 <diatsilva007@gmail.com>
2024-10-03 23:39:28 -0300

    feat: updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,21 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen 
-      name="index" 
+      <Stack.Screen
+        name="index"
         options={{
           headerShown: false,
         }}
       />
-        <Stack.Screen 
-      name="step/index" 
+      <Stack.Screen
+        name="step/index"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="create/index"
         options={{
           headerShown: false,
         }}
--- app/create/index.tsx
+++ app/create/index.tsx
@@ -1,7 +1,32 @@
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  ScrollView
+} from 'react-native';
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { colors } from '../../constants/colors';
+import { Header } from '@/components/header';
 
 export default function create() {
- return (
-   <View/>
+  return (
+    <View style={styles.container}>
+      <Header 
+        step='Passo 2'
+        title='Finalizando dieta'
+      />
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background
+  },
+})
